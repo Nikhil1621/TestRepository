6f76fddebc58bb3183a8600bfd29ee4d7cdf2610
shindnx <nikhil.shinde@abbott.com>
2019-08-03 20:16:39 +0530

    Angular Routing code changes

--- APM-Start/src/app/product/product-list/product-list.component.ts
+++ APM-Start/src/app/product/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from '../product';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'pm-product-list',
@@ -13,7 +14,9 @@ export class ProductListComponent implements OnInit {
   imageMargin: number = 2;
   showImage: boolean = false;
   filteredProducts: IProduct[];
-  _listFilter : string
+  errorMessage: string;
+
+  _listFilter: string;
   get listFilter(): string {
     return this._listFilter;
   }
@@ -22,45 +25,13 @@ export class ProductListComponent implements OnInit {
     this._listFilter = value;
     this.filteredProducts = this._listFilter ? this.performFilter(this._listFilter) : this.products;
   }
-  products: IProduct[] = [
-    {
-      "productId": 1,
-      "productName": "Leaf Rake",
-      "productCode": "GDN-0011",
-      "releaseDate": "March 19, 2016",
-      "description": "Leaf rake with 48-inch wooden handle.",
-      "price": 19.95,
-      "starRating": 3.2,
-      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-    },
-    {
-      "productId": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2016",
-      "description": "15 gallon capacity rolling garden cart",
-      "price": 32.99,
-      "starRating": 4.2,
-      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-    },
-    {
-      "productId": 5,
-      "productName": "Hammer",
-      "productCode": "TBX-0048",
-      "releaseDate": "May 21, 2016",
-      "description": "Curved claw steel hammer",
-      "price": 8.9,
-      "starRating": 4.8,
-      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-    }
-  ]
+  products: IProduct[] = [];
 
   toggleImage(): void {
     this.showImage = !this.showImage;
   }
-  constructor() {
-    this.filteredProducts = this.products;
-    this._listFilter = 'cart';
+  constructor(private productService: ProductService) {
+    
   }
 
   performFilter(filterBy: string): IProduct[] {
@@ -72,7 +43,17 @@ export class ProductListComponent implements OnInit {
   onRatingClicked(message: string): void {
     this.pageTitle = 'Product List:' + message;
   }
+
   ngOnInit() {
+    this.productService.getProducts().subscribe(
+      products => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+      error => this.errorMessage = <any>error
+
+    );
+   
   }
 
 }
